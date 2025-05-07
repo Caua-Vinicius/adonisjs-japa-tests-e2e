51daa685623d1bae26d0f480795a075933c9eb4e
Cauã Vinicius Gomes Monteiro <cauavinicius.contato@gmail.com>
2025-05-07 14:45:14 -0300

    feat: add products controller and routes

--- hello-world/start/routes.ts
+++ hello-world/start/routes.ts
@@ -8,9 +8,15 @@
 */
 
 import router from '@adonisjs/core/services/router'
+const ProductsController = () => import('../app/controllers/products_controller.js')
 
 router.get('/', async () => {
   return {
     hello: 'world',
   }
 })
+router.post('/products', [ProductsController, 'create'])
+router.get('/products', [ProductsController, 'getAll'])
+router.get('/products/:id', [ProductsController, 'getById'])
+router.put('/products/:id', [ProductsController, 'update'])
+router.delete('/products/:id', [ProductsController, 'delete'])
